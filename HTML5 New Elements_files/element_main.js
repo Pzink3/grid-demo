6e8b920730f60e08e1cbe5aa4412c6f89ee2dccd
Parker Zink <parker.zink@gmail.com>
2014-09-28 18:01:57 -0700

    More progress that I recently did so far

---
+++ HTML5 New Elements_files/element_main.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+		<html><body>
+		<script type="text/javascript">
+		<!--
+			if ((document.layers) || (document.getElementById && !document.all)) {
+                var pages = history.length-1;
+			} else {
+                var pages = history.length;
+			}
+			if(pages > 0) {
+				history.back();
+			} else {
+				window.location = 'about:blank';
+			}
+		-->
+		</script>
+		</body></html>
